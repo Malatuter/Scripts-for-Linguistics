e96bd2cf29e6c717e1cccf688978828acef0c297
Malatuter <129164187+Malatuter@users.noreply.github.com>
2023-09-06 16:19:53 +0800

    Update chooseFood.py

--- chooseFo/chooseFood.py
+++ chooseFo/chooseFood.py
@@ -1,6 +1,25 @@
 import random
-with open("foodlist.txt") as f:
-	content = f.readlines()
-choice = random.randint(0,len(content))
+
+#每行仅限添加一个选项
+foodlist = '''泰国菜
+东南亚
+alfafa
+美心
+SU
+谭仔
+彩面
+日之苑
+阿一猪扒
+绿野仙踪
+西宝城日料
+萨莉亚
+...去深圳
+'''
+
+
+
+content = foodlist.split("\n")
+#print(content)
+choice = random.randint(0,len(content)-1)
 print("今天吃",content[choice])
 
